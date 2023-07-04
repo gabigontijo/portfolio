e1abccea8d79680163f052c8f8f102653aea6a19
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2023-07-04 19:30:11 +0100

    init

---
+++ src/pages/CardDev.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from '../components/Header'
+import Resume from '../components/Resume';
+import Skillset from '../components/Skillset';
+import Projects from '../components/Projects';
+import Footer from '../components/Footer';
+import Contact  from '../components/Contact';
+import '../style/Footer.css';
+
+export default function CardDev() {
+    return (
+        <div className="main_body">
+            <Header />
+            <Resume />
+            <Skillset />
+            <Projects />
+            <Contact />
+            <Footer />
+        </div>
+    );
+}
